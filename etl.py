0090d900b1c68455c7c406cb100e98385b1ea187
Pooja V <63975461+Poojakatta@users.noreply.github.com>
2023-07-12 17:23:52 +0530

    added docstrings to the functions

--- etl.py
+++ etl.py
@@ -4,12 +4,38 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load data from S3 bucket to staging tables.
+
+    Args:
+        cur: The cursor object.
+        conn: The connection object.
+
+    Returns:
+        None.
+
+    Raises:
+        Any exceptions raised by the `cur.execute()` or `conn.commit()` methods.
+    """
     for query in copy_table_queries:        
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data staging tables into final fact and dimension tables.
+
+    Args:
+        cur: The cursor object.
+        conn: The connection object.
+
+    Returns:
+        None.
+
+    Raises:
+        Any exceptions raised by the `cur.execute()` or `conn.commit()` methods.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
